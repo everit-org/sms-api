4bd8b9223c19f0a4d1802ca55c45309a09e9213d
attila.kiss <attila.kiss@everit.biz>
2015-07-08 15:42:01 +0200

    refactor

--- src/main/java/org/everit/sms/SMSSender.java
+++ src/main/java/org/everit/sms/SMSSender.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.everit.sms.api;
+package org.everit.sms;
 
 /**
  * Interface to send SMS.
